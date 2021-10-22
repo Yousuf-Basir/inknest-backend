4c23daf31ae68422311e522132df4d5ce321cb89
yousuf-basir <gourabxz@gmail.com>
2021-10-22 22:07:33 +0600

    bug fix

--- middlewares/HandleShelf/createShelf.middleware.js
+++ middlewares/HandleShelf/createShelf.middleware.js
@@ -4,12 +4,14 @@ const {v4: uuidv4} = require("uuid");
 const createShelf = async (req, res, next) => {
     // get shelfOwner UID from authenticateToken middleware
     const {userUid} = req.user;
+    
+    if(!req.body.shelfName || !req.body.isOpen || !req.body.shelfDescription){
+        return next();
+    }
+
     // get shelf properties from req.body using POST
     const {shelfName, isOpen,  shelfDescription} = req.body;
 
-    if(!shelfName || !isOpen || !shelfDescription){
-        return next();
-    }
 
     const shelfUid = uuidv4();
 
